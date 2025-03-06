82f87f471c4e2f4b30235a0c5deb9d1cae7c83f7
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-07 01:37:58 +0330

    add code splitting component example

---
+++ code-splitting.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, h, defineAsyncComponent } from 'vue';
+const CodeSplitExampleComponent = defineAsyncComponent(() =>
+    import('@/components/CodeSplitExampleComponent.vue')
+)
+
+// const CodeSplitExampleComponent = defineAsyncComponent({
+//     loader: () => import('@/components/CodeSplitExampleComponent.vue'),
+//     loadingComponent: h('div', 'Loading...') /* shows while loading */,
+//     errorComponent: h('div', 'Error!') /* shows if there's an error */,
+//     delay: 1000 /* delay in ms before showing loading component */,
+//     timeout: 3000 /* timeout after this many ms */,
+// })
+
+const show = ref(false);
+
+</script>
+<template>
+    <h1 class="text-3xl mb-3">Code Splitting End</h1>
+    <p>Hello Code Splitting Page End</p>
+    <button class="btn mb-3" @click="show = !show">Toggle Code Splitting Example</button>
+    <CodeSplitExampleComponent v-if="show" />
+</template>
