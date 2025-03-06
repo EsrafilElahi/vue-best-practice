608212fd9e16f1c498379c2b7f404ddedcf9a671
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-07 01:07:10 +0330

    add some best practice components and configs

---
+++ crud-pinia/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+  <input type="checkbox" :id="nameModal" class="modal-toggle" />
+  <label :for="nameModal" class="modal cursor-pointer">
+    <label class="modal-box relative">
+      <h3 class="font-bold text-lg">
+        <slot name="title" />
+      </h3>
+      <p class="py-4">
+        <slot name="content" />
+      </p>
+      <div class="modal-action">
+        <slot name="button" />
+      </div>
+    </label>
+  </label>
+</template>
+
+<script setup>
+/**
+  props
+**/
+
+const props = defineProps({
+  nameModal: {
+    type: String,
+    default: 'my-modal',
+  },
+})
+</script>
+
+<!-- usage -->
+<Modal>
+  <template #title> Logout </template>
+  <template #content>Are you sure You want to leave ? </template>
+  <template #button
+    ><button @click="onLogout" for="my-modal" class="btn btn-primary">
+      Logout
+    </button>
+  </template>
+</Modal>
