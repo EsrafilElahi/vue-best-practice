fc5a99b6854f9705c3bf5c1fcd7ac2b6d0183789
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-07 01:09:19 +0330

    add some usage of ErrorBoundaryEnd compo

--- ErrorBoundaryEnd.vue
+++ ErrorBoundaryEnd.vue
@@ -43,3 +43,18 @@ const slotName = computed(() => (error.value ? "error" : "default"));
 <RandomErrorWithSlots>
   <!-- محتوای اصلی -->
 </RandomErrorWithSlots>
+
+
+<template>
+  <h1 class="text-2xl font-bold mb-5">
+    Lesson 5 - Error Boundary Component End
+  </h1>
+  <!-- or NuxtErrorBoundary -->
+  <ErrorBoundaryEnd>
+    <RandomErrorParent />
+    <template #error="{ error, clearError }">
+      <p>Error Template: {{ error }}</p>
+      <button class="btn btn-primary" @click="clearError">Clear Error</button>
+    </template>
+  </ErrorBoundaryEnd>
+</template>
