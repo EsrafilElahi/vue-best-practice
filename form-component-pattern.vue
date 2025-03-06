a30b378613e1c21aa825b21bb7be91182f57ecff
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-07 01:50:43 +0330

    add UserForm and how to use it

---
+++ form-component-pattern.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const user = ref(null)
+</script>
+<template>
+  <div>
+    <PageHeading :number="5">Form Component Pattern</PageHeading>
+
+    <UserForm v-model="user" />
+
+    <pre>{{ user }}</pre>
+  </div>
+</template>
