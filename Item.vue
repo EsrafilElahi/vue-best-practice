756a92d17cd6661bb422466d637cd6d7eb8e7b4c
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-21 20:02:08 +0330

    change item.vue file

--- Item.vue
+++ Item.vue
@@ -1,75 +1,90 @@
 <script setup>
-const wrapper = ref()
-defineExpose({ wrapper }) // expose the wrapper to parent component, below see usage of this
+const wrapper = ref();
+defineExpose({ wrapper }); // expose the wrapper to parent component, below see usage of this
 
 const props = defineProps({
-	action: {
-		type: Object,
-	},
-	isFocused: {
-		type: Boolean,
-		default: false,
-	},
-})
+  action: {
+    type: Object,
+  },
+  isFocused: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
 <template>
-	<Flex ref="wrapper" align="center" justify="between" :class="[$style.wrapper, isFocused && $style.focused]">
-		<Flex align="center" gap="8" :class="$style.left">
-			<Icon :name="action.icon" size="16" color="primary" :class="$style.icon" />
+  <Flex
+    ref="wrapper"
+    align="center"
+    justify="between"
+    :class="[$style.wrapper, isFocused && $style.focused]"
+  >
+    <Flex align="center" gap="8" :class="$style.left">
+      <Icon
+        :name="action.icon"
+        size="16"
+        color="primary"
+        :class="$style.icon"
+      />
 
-			<Text size="13" weight="500" height="13" color="primary" :class="$style.title">
-				{{ action.title }}
-			</Text>
-		</Flex>
+      <Text
+        size="13"
+        weight="500"
+        height="13"
+        color="primary"
+        :class="$style.title"
+      >
+        {{ action.title }}
+      </Text>
+    </Flex>
 
-		<Text v-if="action.subtitle" size="12" weight="600" color="tertiary">
-			{{ action.subtitle }}
-		</Text>
-	</Flex>
+    <Text v-if="action.subtitle" size="12" weight="600" color="tertiary">
+      {{ action.subtitle }}
+    </Text>
+  </Flex>
 </template>
 
 <style module>
 .wrapper {
-	height: 34px;
-	cursor: pointer;
-	padding: 0 10px;
+  height: 34px;
+  cursor: pointer;
+  padding: 0 10px;
 
-	&:hover {
-		background: rgba(247, 147, 26, 0.1);
-		border-radius: 4px 4px 4px 4px;
-	}
+  &:hover {
+    background: rgba(247, 147, 26, 0.1);
+    border-radius: 4px 4px 4px 4px;
+  }
 
-	&:active {
-		background: var(--op-8);
-	}
+  &:active {
+    background: var(--op-8);
+  }
 
-	&.focused {
-		background: rgba(247, 147, 26, 0.1);
-		border-radius: 4px 4px 4px 4px;
-		border: 1px solid #F7931A;
-	}
+  &.focused {
+    background: rgba(247, 147, 26, 0.1);
+    border-radius: 4px 4px 4px 4px;
+    border: 1px solid #f7931a;
+  }
 }
 
 .left {
-	min-width: 0;
+  min-width: 0;
 }
 
 .icon {
-	box-sizing: content-box;
+  box-sizing: content-box;
 
-	border-radius: 5px;
-	background: var(--op-10);
+  border-radius: 5px;
+  background: var(--op-10);
 
-	padding: 4px;
+  padding: 4px;
 }
 
 .title {
-	overflow: hidden;
-	text-overflow: ellipsis;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
 
 <!-- usage of expose -->
-import Item from "./Item.vue"
-console.log(Item.wrapper)
+import Item from "./Item.vue" console.log(Item.wrapper)
