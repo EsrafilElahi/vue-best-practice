7d881b694b8743192d97508c9ae11f8b8677bee6
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-21 20:15:58 +0330

    update slot file

--- sendDataFromSlot.vue
+++ sendDataFromSlot.vue
@@ -1,22 +1,72 @@
-// child file
-<ul>
-  <li v-for="item in items">
-    <slot name="item" v-bind="item"></slot>
-  </li>
-</ul>
+// -------- child file --------
+<script setup>
+import { ref } from 'vue'
 
-// parent file
-<template #item="{ body, username, likes }">
-  <div class="item">
-    <p>{{ body }}</p>
-    <p>by {{ username }} | {{ likes }} likes</p>
-  </div>
+const props = defineProps(['api-url', 'per-page'])
+
+const items = ref([])
+
+// mock remote data fetching
+setTimeout(() => {
+  items.value = [
+    { body: 'Scoped Slots Guide', username: 'Evan You', likes: 20 },
+	  { body: 'Vue Tutorial', username: 'Natalia Tepluhina', likes: 10 }
+  ]
+}, 1000)
+</script>
+
+<template>
+  <ul>
+    <li v-if="!items.length">
+      Loading...
+    </li>
+    <li v-for="item in items">
+      <slot name="item" v-bind="item"/>
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 5px;
+    background: linear-gradient(315deg, #42d392 25%, #647eff);
+  }
+  li {
+    padding: 5px 20px;
+    margin: 10px;
+    background: #fff;
+  }
+</style>
+
+
+// -------- parent file --------
+<script setup>
+import FancyList from './FancyList.vue'
+</script>
+
+<template>
+  <FancyList api-url="url" :per-page="10">
+    <template #item="{ body, username, likes }">
+      <div class="item">
+        <p>{{ body }}</p>
+        <p class="meta">by {{ username }} | {{ likes }} likes</p>
+      </div>
+    </template>
+  </FancyList>
+</template>
+
+<style scoped>
+.meta {
+  font-size: 0.8em;
+  color: #42b883;
+}
+</style>
 
 
 
 // ==================== another mesla ======================
-// child component
+// -------- child component --------
 <script setup>
 const greetingMessage = 'hello'
 </script>
@@ -28,7 +78,7 @@ const greetingMessage = 'hello'
 </template>
 
 
-// parent component
+// -------- parent component --------
 <script setup>
 import MyComponent from './MyComponent.vue'
 </script>
