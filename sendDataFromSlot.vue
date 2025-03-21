9b335865e79ef60db155f57261ac932fafe79419
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-21 20:12:58 +0330

    add slot file

---
+++ sendDataFromSlot.vue
@@ -0,0 +1,14 @@
+// child file
+<ul>
+  <li v-for="item in items">
+    <slot name="item" v-bind="item"></slot>
+  </li>
+</ul>
+
+// parent file
+<template #item="{ body, username, likes }">
+  <div class="item">
+    <p>{{ body }}</p>
+    <p>by {{ username }} | {{ likes }} likes</p>
+  </div>
+</template>
