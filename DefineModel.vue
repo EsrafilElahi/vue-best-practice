8a9fa4750bcfcc4f8ed2324f522a68833fba7e38
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-21 20:21:07 +0330

    add defineModel file

---
+++ DefineModel.vue
@@ -0,0 +1,21 @@
+// ============== child ==============
+<template>
+  <input v-model="model" />
+</template>
+
+<script setup>
+import { defineModel } from "vue";
+
+const model = defineModel();
+</script>
+
+// ============== parent ==============
+<template>
+  <ChildComponent v-model="parentModel" />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const parentModel = ref("Initial Value");
+</script>
